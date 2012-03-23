82b107d6114a88957974e79fefae8da5c9e0373d
Jack Dwyer <jackjack.dwyer@gmail.com>
2012-03-23 13:42:42 +1100

    xied

---
+++ WorkerRollingAverageSubtraction.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2.7
+"""
+Jack dywer
+23 march 2012
+
+Averages the Static Image, and subtracts the averaged Buffer Average
+"""
+
+class WorkerRollingAverageSubtraction():
+    
+    def __init__(self):
+        self.allInt
