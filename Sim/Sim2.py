7a8c0cc66fa7bd20f21b161c478f90da71d793d6
Jack Dwyer <jackjack.dwyer@gmail.com>
2012-04-01 08:38:23 +1000

    Beamline/Epics simulator - needs to be updated to include new PV's and filepaths

---
+++ Sim/Sim2.py
@@ -0,0 +1,101 @@
+"""
+Jack Dwyer
+17/02/2011
+simulator that writes lines out to a 'live log'
+
+20/02/2011
+- Moved pre generated dat filles to simulation location
+- setup the log file generation
+TODO 
+caput epics value.
+
+23/12/2012
+Will create folder structure from the entered data
+
+------
+Checks directory listing of scatterbrain .dat files, once a new file has appeared.
+appends a line to the 'live log' and pushes '1' to a yet to be decided PV in EPICS 
+the actual engine is listing to .
+"""
+import time
+import epics
+import glob
+import shutil
+import os
+from LogLine import LogLine
+
+#Create User Directory:
+def ensure_dir(f):
+    d = os.path.dirname(f)
+    if not os.path.exists(d):
+        os.makedirs(d)
+
+def makeLog(user):
+    #make the log file
+    liveLogFile = open("/home/dwyerj/sim/"+user+"/livelogfile.log", "w")
+    liveLogFile.close()
+
+def main(user):
+    dir = "/home/dwyerj/sim/"+user+"/"
+    ensure_dir(dir)
+    makeLog(user)
+    epics.caput("13SIM1:cam1:NumImages.VAL", 0, wait=True)
+    
+
+def run(user):
+      
+    #setup log generation 
+    logFileName = "livelogfile.log" #actual real log file prevous generated in another experiment
+    log = open(logFileName)
+    line = log.readline()
+    
+    
+    print "******** Simulation Commenced **********"
+    
+    
+    #list files in directory
+    datFiles = glob.glob('/home/dwyerj/legitData/NK_dat/*.dat')
+    
+    x = 0 #index for datFileList
+    time.sleep(2)
+    while True:
+        lineData = LogLine(line)
+        fileLoc = lineData.data["ImageLocation"]
+        print file
+        liveLogFile = open("/home/dwyerj/sim/"+user+"/livelogfile.log", "a")
+        epics.caput("13SIM1:cam1:NumImages.VAL", 0, wait=True)
+        print epics.caget("13SIM1:cam1:NumImages_RBV")
+        #get just filename
+        
+        fileName = fileLoc.split('/')
+        fileName = fileName[-1]
+        fileName = fileName.split('.')
+        fileName = fileName[0] + ".dat"
+        actualFileLoc = '/home/dwyerj/legitData/NK_dat/'+fileName
+
+        location = '/home/dwyerj/sim/'+user+'/'+fileName #new location of datfiles
+        shutil.copy(actualFileLoc, location)
+        print "Dat File: " + fileName + " Generated"
+        
+        #write a line out to the 'live' log    
+        liveLogFile.write(line)
+        liveLogFile.close()
+        print "Line: " + line +  "- written to live log"   
+        
+        line = log.readline()
+            
+        #throw some data out to epics to let the actual python script know that there has been some images/shit happened
+        epics.caput("13SIM1:cam1:NumImages.VAL", 100, wait=True)              
+                       
+        time.sleep(2)    
+        
+        #step up index of dat files
+        x = x + 1
+    
+if __name__ == "__main__":
+    user = raw_input("Please Enter User: ")
+    main(user)
+    raw_input("Press Enter to Start Simulation...")
+    run(user)
+    
+
