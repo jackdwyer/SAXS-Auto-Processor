9a596111320f6a443711503b7240b0632c6f09b7
Jack Dwyer <jackjack.dwyer@gmail.com>
2012-04-01 12:58:18 +1000

    fixed unit tests

--- WorkerStaticImage.py
+++ WorkerStaticImage.py
@@ -63,10 +63,10 @@ if __name__ == "__main__":
     worker = WorkerStaticImage()
     
     if len(sys.argv) > 1 and sys.argv[1] == "tests":
-        worker.writeFile("TestWrite")
-        
-        
-    
+        buf = [1,1,1]
+        dat = DatFile.DatFile("Sim/data/test.dat")
+        worker.run(dat, buf)      
+            
     else:
         context = zmq.Context()
 
