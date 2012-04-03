e04352dd8d152327185bcf2fc30438da52624899
jackdwyer <jackjack.dwyer@gmail.com>
2012-04-03 17:14:52 +1000

    Building a new Engine to simplify for use on beamline, will have a yaml config

---
+++ Engine2.py
@@ -0,0 +1,14 @@
+"""
+Jack Dwyer
+03/April/2012
+
+To simplify the engine
+
+YAML Config for:
+ - Database settings
+ - Which workers to spool up, with dynamically created ports
+ - WorkerPipelines (for after the creation of the subtracted average)
+
+Engine2 will replace Engine1 very soon 
+ 
+"""     
