0e1a64eef90e3b264fa99a280246dad47d5adb21
Jack Dwyer <jackjack.dwyer@gmail.com>
2012-04-25 22:14:07 +1000

    exec xrandr --output HDMI-0 --right- moved workers

---
+++ Workers/WorkerDB.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python2.7
+"""
+Jack dywer
+20 march 2012
+"""
+#TODO: Fix up sample Typpe, eg write to another table of subtracted types.
+
+import zmq
+from CommonLib import TableBuilder
+from CommonLib import Logger
+import MySQLdb as mysql
+from Worker import Worker
+
+
+
+class WorkerDB(Worker):
+    
+    def cheese(self, host, user, password):
+        
+        context = zmq.Context()
+        dbWriter = context.socket(zmq.PULL)
+        dbWriter.connect("tcp://127.0.0.1:"+self.port)
+        
+        try:
+            while True:
+                #Will be first sent, what to write/do
+                filter = dbWriter.recv()
+                if (str(filter) == "user"):
+                    user = dbWriter.recv()
+                    Logger.log(worker.name, "Recieved Command to build Database for user: " + str(user))
+                    worker.forceDBCreation(user)
+                    
+                if (str(filter) == "Experiment"):
+                    experiment = dbWriter.recv()
+                    Logger.log(worker.name, "Building experiment table: " + str(experiment))
+                    worker.buildExperimentTable(str(experiment))
+                
+                if (str(filter) == "logLine"):
+                    Logger.log(worker.name, "Writing LogLine to DB")
+                    logLine = dbWriter.recv_pyobj()
+                    worker.writeLogToDB(logLine)
+                    Logger.log(worker.name, "LogLine written")
+    
+                    
+                if (str(filter) == "ImageLocation"):
+                    location = dbWriter.recv()
+                    Logger.log(worker.name, "Image Location recieved, writing to DB")
+                    worker.writeImageLocation(str(location))
+                    Logger.log(worker.name, "Location written: ", str(location))
+
+        except KeyboardInterrupt:
+            pass
+        
+        
+        #for logging
+        self.name = "WorkerDB"
+
+        
+        #General SQL setup
+        self.host = host
+        self.dbUser = user
+        self.password = password
+        
+        
+        
+        self.user = ""
+        self.experiment = ""
+        self.logTable = "" 
+        self.imageTable = ""
+        
+        Logger.log(self.name, "Generated")
+
+    
+    def forceDBCreation(self, user):
+        
+        Logger.log(self.name, "Forcing Database Creation")
+        self.user = user
+        try:
+            db = mysql.connect(user=self.dbUser, host=self.host, passwd=self.password)
+            c = db.cursor()
+            cmd = "CREATE DATABASE IF NOT EXISTS " + str(self.user) + ";"
+            c.execute(cmd)      
+            Logger.log(self.name, "Database Created for user: " + str(self.user))
+        except Exception:
+            Logger.log(self.name, "Database CREATION FAILED for user:" + str(self.user))
+            raise
+    
+    
+    def buildExperimentTable(self, experiment):
+        self.experiment = experiment
+        collumAttributes = ['I0', 'NumericTimeStamp', 'WashType', 'FilePluginDestination', 'TimeStamp', 'Energy', 'NORD', 'SampleType', 'It', 'SampleTableX', 'SampleTableY', 'Temperature2', 'Temperature1', 'WellNumber', 'Ibs', 'exptime', 'FilePluginFileName', 'ImageLocation']
+        self.logTable = TableBuilder.TableBuilder(self.user, self.experiment, collumAttributes)
+        
+        #This needs to bs fixed to support different sample types
+        imageLoc = ['subtracted_location']
+        self.imageTable = TableBuilder.TableBuilder(self.user, 'Images', imageLoc)
+        
+    def writeLogToDB(self, logLine):
+        self.logTable.addData(logLine.data)
+        
+    def writeImageLocation(self, location):
+        d = { "subtracted_location" : location }
+        self.imageTable.addData(d)
+    
+
+
+
+if __name__ == "__main__":
+    worker = WorkerDB()
+    
+    
+    
+    context = zmq.Context()
+    dbWriter = context.socket(zmq.PULL)
+    dbWriter.connect("tcp://127.0.0.1:7884")
+    
+    try:
+        while True:
+            #Will be first sent, what to write/do
+            filter = dbWriter.recv()
+            if (str(filter) == "user"):
+                user = dbWriter.recv()
+                Logger.log(worker.name, "Recieved Command to build Database for user: " + str(user))
+                worker.forceDBCreation(user)
+                
+            if (str(filter) == "Experiment"):
+                experiment = dbWriter.recv()
+                Logger.log(worker.name, "Building experiment table: " + str(experiment))
+                worker.buildExperimentTable(str(experiment))
+            
+            if (str(filter) == "logLine"):
+                Logger.log(worker.name, "Writing LogLine to DB")
+                logLine = dbWriter.recv_pyobj()
+                worker.writeLogToDB(logLine)
+                Logger.log(worker.name, "LogLine written")
+
+                
+            if (str(filter) == "ImageLocation"):
+                location = dbWriter.recv()
+                Logger.log(worker.name, "Image Location recieved, writing to DB")
+                worker.writeImageLocation(str(location))
+                Logger.log(worker.name, "Location written: ", str(location))
+                
+
+    except KeyboardInterrupt:
+        pass
---
+++ Workers/WorkerRollingAverageSubtraction.py
@@ -0,0 +1,136 @@
+"""
+Jack Dwyer
+
+"""
+
+from threading import Thread
+import zmq
+import sys
+import time
+
+from Core.Logger import log
+from Core import DatFile
+from Core import DatFileWriter
+
+from Worker import Worker
+
+class WorkerStaticImage(Worker):
+    def __init__(self):
+        Worker.__init__(self, "WorkerStaticImage")
+        
+        self.subtractedDatIntensities = []
+        self.subtractedDatq = []
+        self.subtractedErrors = []
+        
+        #For adding to be cleared when new user/sample
+        self.addToClearList(self.subtractedDatIntensities)
+        self.addToClearList(self.subtractedDatq)
+        self.addToClearList(self.subtractedErrors)
+
+    def process(self, filter):
+        if (filter == "image"):
+            self.datFile = self.pull.recv_pyobj()
+            log(self.name, "Static Image Received")
+            
+            if (self.needBuffer):
+                self.reqBuffer.send("bufferRequest")
+                self.aveBuffer = self.reqBuffer.recv_pyobj()
+                self.needBuffer = False
+                self.average()
+                self.imageSubtraction()
+            else:
+                self.average()
+                self.imageSubtraction()
+                
+    def average(self):
+        self.allIntensities.append(self.datFile.intensities)
+        self.allQ.append(self.datFile.q)
+        self.allErrors.append(self.datFile.errors)
+
+        #averaging out
+        self.aveIntensities = self.ave.average(self.allIntensities)
+        self.aveQ = self.ave.average(self.allQ)
+        self.aveErrors = self.ave.average(self.allErrors)
+        
+        
+
+        Logger.log(self.name, "Averaging Completed")
+        
+        self.subtract(aveBuffer)
+        
+        self.datWriter.writeFile("/home/ics/jack/beam/", self.name, { 'q': self.aveQ, 'i' : self.aveIntensities, 'errors':self.aveErrors})
+        
+        
+
+
+
+
+
+    def imageSubtraction(self):
+        subtractedDatIntensities = []
+        subtractedDatq = []
+        subtractedErrors = []
+        for i in range(len(self.dateFile.intensities)):
+            #Intensities
+            value = self.datFile.intensities[i] - self.aveBuffer[i]
+            subtractedDatIntensities.insert(i, value)
+            #Q Values
+            subtractedDatq.insert(i, self.datFile.q[i])
+            subtractedErrors.insert(i, self.datFile.errors[i])
+        
+        
+        self.subtractedDatIntensities = subtractedDatIntensities
+        self.subtractedDatq = subtractedDatq
+        self.subtractedErrors = subtractedErrors
+
+        fileName = datFile.getFileName()
+        
+        self.datWriter.writeFile(self.absolutePath + "sub/raw_sub" , str(fileName) , { 'q' : self.subtractedDatq, 'i' : self.subtractedDatIntensities, 'errors' : self.subtractedErrors})
+        log(self.name, "Static Image Written ->" + fileName)
+
+
+        
+
+
+
+
+if __name__ == "__main__":
+    pushPort = 4000
+    reqPort = 8000
+    context = zmq.Context()
+    replyPass = False
+
+    print "TEST 1 - ONLY PUSH/PULL"
+    #Test 1 - Only a pull socket
+    b = WorkerStaticImage()
+    t = Thread(target=b.connect, args=(pushPort, False))
+    t.start()
+
+    
+    testPush = context.socket(zmq.PUSH)
+    testPush.bind("tcp://127.0.0.1:"+str(pushPort))
+    testPush.send("clear")
+    time.sleep(0.1)
+    testPush.close()
+
+
+    #Test 2
+    print "TEST 2 - ONLY REQ/RECV"
+    b = Worker()
+    t = Thread(target=b.connect, args=(pushPort, reqPort))
+    t.start()
+    
+    testReq = context.socket(zmq.REQ)
+    testReq.connect("tcp://127.0.0.1:"+str(reqPort))
+    testReq.send("testReply")
+    testReply = testReq.recv_pyobj()
+    
+    if (testReply == "testReply"):
+        replyPass = True
+
+    testReq.close()
+    
+    if (replyPass):
+        print "TEST OVER - Succeeded"
+    else:
+        print "TEST OVER - Failed with reply"
