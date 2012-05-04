b97823cc3ce12d6690e987b6b25695d1e04cb2ad
Jack Dwyer <jackjack.dwyer@gmail.com>
2012-05-04 13:20:54 +1000

    Does log, and write to standard out.

--- Core/Logger.py
+++ Core/Logger.py
@@ -1,5 +1,6 @@
 def log(name, message):
     logFile = "engine.log"
     formatting  = '%34s %10s %1s'
+    print formatting % ('['+name+']','--', message+"\n"),
     f = open(logFile, 'a')
     f.write(formatting % ('['+name+']','--', message+"\n"))#Needed for string formatting
