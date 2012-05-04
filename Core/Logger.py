c060be05ac6031bc9a66ccfa76b93073f922c152
Jack Dwyer <jackjack.dwyer@gmail.com>
2012-05-04 11:55:29 +1000

    Changed to write out to a log file

--- Core/Logger.py
+++ Core/Logger.py
@@ -1,3 +1,5 @@
 def log(name, message):
+    logFile = "engine.log"
     formatting  = '%34s %10s %1s'
-    print formatting % ('['+name+']','--', message+"\n"), #Needed for string formatting
+    f = open(logFile, 'a')
+    f.write(formatting % ('['+name+']','--', message+"\n"))#Needed for string formatting
