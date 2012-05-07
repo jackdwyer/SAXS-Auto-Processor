0ea26fc365cd5108079763b3ba6257c5d4453c25
Jack Dwyer <jackjack.dwyer@gmail.com>
2012-05-07 10:22:03 +1000

    Slapped in the current date/time

--- Core/Logger.py
+++ Core/Logger.py
@@ -1,6 +1,10 @@
+import datetime
+
 def log(name, message):
+    now = datetime.datetime.now()
     logFile = "engine.log"
     formatting  = '%34s %10s %1s'
+    print str(now)
     print formatting % ('['+name+']','--', message+"\n"),
     f = open(logFile, 'a')
     f.write(formatting % ('['+name+']','--', message+"\n"))#Needed for string formatting
