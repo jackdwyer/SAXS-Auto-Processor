a6e8a03bd7cfcf4700dd98c7e8dd8ca09985bb2c
Jack Dwyer <jackjack.dwyer@gmail.com>
2012-05-15 14:30:15 +1000

    Cleaned up the formatting a little

--- Core/Logger.py
+++ Core/Logger.py
@@ -6,11 +6,19 @@ def log(name, message):
     
     
     logFile = "engine.log"
-    formatting  = '%2s %2s %35s %10s'
+    formatting  = '%26s %0s %10s %100s'
     #print str(now)
-    print formatting % (str(now), ' -- ['+name+']','--', message+"\n"),
-    f = open(logFile, 'a')
-    f.write(formatting % (str(now), '-- ['+name+']','--', message+"\n"))#Needed for string formatting
+    print "%26s -- [%40s] %10s %s" % (str(now), name, "--", 
+                                    message)
+    #print formatting % (str(now), ' -- ['+name+']','--', message+"\n"),
+    #f = open(logFile, 'a')
+    #f.write(formatting % (str(now), '-- ['+name+']','--', message+"\n"))#Needed for string formatting
 
 if __name__ == "__main__":
     log("Jack", "UNIT TEST MESSAGE")
+    log("Engine", "Engine Started")
+    log("WorkerBufferAverage", "Generated")
+    log("WorkerStaticImage", "Generated")
+    log("WorkerRollingAverageSubtraction", "Generated")
+    log("Engine", "Connected -> BufferRequest")
+
