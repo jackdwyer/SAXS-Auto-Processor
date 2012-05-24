71b9474cd0c85fc90444873f7d39e59b064795de
Jack Dwyer <jackjack.dwyer@gmail.com>
2012-05-24 19:45:25 +1000

    added a small amount of lines

--- Core/OutLierRejection.py
+++ Core/OutLierRejection.py
@@ -13,16 +13,28 @@ class OutLierRejection():
         print "out lier rejection created"
 
     def process(self, datFiles):
-        #datFiles = {} (dictionary)
+        #If length is 1, can not check
         if (len(datFiles) == 1):
             return False
         else:
-            
+            print "iql"
+            for dat in datFiles:
+#                IQL 
 
-
-    def checkIHQ(datFiles):
 
 
 
 if __name__ == '__main__':
     print "Running - Outlier Rejection"
+    dat1 = DatFile.DatFile("testData/empty_cap2a_chk_0001.dat")
+    dat2 = DatFile.DatFile("testData/empty_cap2a_chk_0002.dat")
+    dat3 = DatFile.DatFile("testData/empty_cap2a_chk_0003.dat")
+    dat4 = DatFile.DatFile("testData/empty_cap2a_chk_0004.dat")
+    
+    o = OutLierRejection()
+    o.process([dat1, dat2, dat3, dat4])
+    
+    print dat1.findILQ()
+    print dat2.findILQ()
+    print dat3.findILQ()
+    print dat4.findILQ()
