5d272f8cf07769a512b6bd6cbdee66dccad6a66a
Jack Dwyer <jackjack.dwyer@gmail.com>
2012-05-24 18:00:39 +1000

    Creating outlierrejection, should be just a funcitonal class

---
+++ Core/OutLierRejection.py
@@ -0,0 +1,28 @@
+"""
+Jack Dwyer
+Outlier rejection
+pass a list of average high Q and averaged low Q.
+
+Will need to pass a dictionary of all dat files to be checked against
+"""
+
+import DatFile
+
+class OutLierRejection():
+    def __init__(self):
+        print "out lier rejection created"
+
+    def process(self, datFiles):
+        #datFiles = {} (dictionary)
+        if (len(datFiles) == 1):
+            return False
+        else:
+            
+
+
+    def checkIHQ(datFiles):
+
+
+
+if __name__ == '__main__':
+    print "Running - Outlier Rejection"
