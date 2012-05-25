da233158b4461cd58273dc06f2946047ca10d889
Jack Dwyer <jackjack.dwyer@gmail.com>
2012-05-25 16:33:46 +1000

    Need to finish correct pattern rejection

--- Core/OutLierRejection.py
+++ Core/OutLierRejection.py
@@ -17,10 +17,16 @@ class OutLierRejection():
         if (len(datFiles) == 1):
             return False
         else:
-            print "iql"
+            ILQ = []
+            IHQ = []
             for dat in datFiles:
-#                IQL 
-
+                ILQ.append(dat.findILQ())
+                IHQ.append(dat.findIHQ())
+            iqlThreshold = (1.1 * min(ILQ))
+            ihqThreshold = (0.95 * max(IHQ))
+            
+            print "IQL Threshold: %s", str(iqlThreshold)
+            print "IHQ Threshold: %s", str(ihqThreshold)
 
 
 
@@ -30,11 +36,19 @@ if __name__ == '__main__':
     dat2 = DatFile.DatFile("testData/empty_cap2a_chk_0002.dat")
     dat3 = DatFile.DatFile("testData/empty_cap2a_chk_0003.dat")
     dat4 = DatFile.DatFile("testData/empty_cap2a_chk_0004.dat")
+    dat5 = DatFile.DatFile("testData/P1B7_HCL_PH3_100_NACL_0585.dat")    
+    dat6 = DatFile.DatFile("testData/P1B7_HCL_PH3_100_NACL_0586.dat")
+    dat7 = DatFile.DatFile("testData/P1B7_HCL_PH3_100_NACL_0587.dat")
+    dat8 = DatFile.DatFile("testData/P1B7_HCL_PH3_100_NACL_0588.dat")
+
     
     o = OutLierRejection()
-    o.process([dat1, dat2, dat3, dat4])
+    o.process([dat1, dat2, dat3, dat4, dat5, dat6, dat7, dat8])
     
     print dat1.findILQ()
     print dat2.findILQ()
     print dat3.findILQ()
     print dat4.findILQ()
+    print "#########"
+    print dat6.findILQ()
+    print dat6.findIHQ()
