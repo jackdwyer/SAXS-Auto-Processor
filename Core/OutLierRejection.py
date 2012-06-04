b44eafadcc75145697dc3564acb4a3407a5db2d9
Jack Dwyer <jackjack.dwyer@gmail.com>
2012-06-04 21:57:42 +1000

    removed some tests

--- Core/OutLierRejection.py
+++ Core/OutLierRejection.py
@@ -46,47 +46,15 @@ if __name__ == '__main__':
     outerlierTest = OutLierRejection()
     print "Running - Outlier Rejection"
     
-    
-    datA = glob.glob('testData/35C_naPhos_nacl_ph7a*')
-    datB = glob.glob('testData/35C_naPhos_nacl_ph7b*')
     datC = glob.glob('testData/35C_naPhos_nacl_ph7c*')
     datD = glob.glob('testData/35C_naPhos_nacl_ph7d*')
-    datE = glob.glob('testData/35C_naPhos_nacl_ph7e*')
-    datF = glob.glob('testData/35C_naPhos_nacl_ph7f*')
-    datG = glob.glob('testData/35C_naPhos_nacl_ph7g*')
 
-    dA = []
-    dB = []
     dC = []
-    dD = []
-    dE = []
-    dF = []
-    dG = []
-    """
-    for files in datA:
-        dA.append(DatFile.DatFile(files))
-    for files in datB:
-        dB.append(DatFile.DatFile(files))
-    """
+
+
     for files in datC:
         dC.append(DatFile.DatFile(files))
     outerlierTest.process(dC)
-    """
-    for files in datD:
-        dD.append(DatFile.DatFile(files))
-    for files in datE:
-        dE.append(DatFile.DatFile(files))
-    for files in datF:
-        dF.append(DatFile.DatFile(files))
-    for files in datG:
-        dG.append(DatFile.DatFile(files))
-    
-    datLists = [dA, dB,dC, dD, dE, dF, dG]
-    for list in datLists:
-        print "DAT LIST 1"
-        outerlierTest.process(list)
-        print "#####"
 
-"""
 
 
