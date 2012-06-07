55822092f8ad2712634eecb1e67f76d9237cdc60
Jack Dwyer <jackjack.dwyer@gmail.com>
2012-06-07 14:55:48 +1000

    generates worker threads, connects up workers and sends out test commands

--- Engine2.py
+++ Engine2.py
@@ -7,11 +7,13 @@ TODO import logging and use that across all workers/core/engine etc
 """
 import logging
 import sys
+import time
 
 import zmq
 import yaml
 
-import imp
+from threading import Thread
+
 
 #from Workers import Worker
 
@@ -30,13 +32,13 @@ class Engine2():
         #Read all configuration settings
         self.setConfiguration(configuration)
 
-
         #ZMQ Class Variables
         self.zmqContext = zmq.Context()
 
-    
         #Instantiate all workers, get them all ready to push out into their own thread and connected up
-        self.instantiateWorkers(self.workers)
+        self.instanceWorkerList = self.instantiateWorkers(self.workers)
+        #Connect up workers
+        self.connectWorkers(self.instanceWorkerList)
     
     
     def setConfiguration(self, configuration):
@@ -62,18 +64,67 @@ class Engine2():
             v = getattr(im, worker)
             x = v()
             instanceDict[worker] = x
+        return instanceDict
+
+    def connectWorkers(self, instanceList):
+        pushPort = 2000
+        pubPort = 1999
+        
+        #Actual Worker Threads
+        workerThreads = {}
+        #Which worker, and which port are they on
+        workerPortLocation = {}
+        self.connectedWorkers = {}
+
+        #Start up a dictionary of threads, so we know where all the workers are        
+        for worker in instanceList:
+            if (instanceList[worker].getName() == "WorkerDB"):
+                workerThreads[worker] = Thread(target=instanceList[worker].connect, args=(pubPort,))                
+            else:
+                workerThreads[worker] = Thread(target=instanceList[worker].connect, args=(pushPort, pubPort,))                            
+                workerPortLocation[worker] = pushPort #So we know where to send commands
+                pushPort = pushPort + 1
+                
+        #Set all workers as Daemon threads (so they all die when we close the application)
+        for workerThread in workerThreads:
+            workerThreads[workerThread].setDaemon(True)
             
-        print instanceDict
+        #Start all the threads
+        for workerThread in workerThreads:
+            workerThreads[workerThread].start()
+            time.sleep(0.1) #short pause to let them properly bind/connect their ports
+            
+        #Set up ZMQ context for each worker
+        for worker in workerPortLocation:
+            workerPortLocation[worker]
+            self.connectedWorkers[worker] = self.zmqContext.socket(zmq.PUSH)
+        
+        #connect workers to the engine
+        for worker in self.connectedWorkers:
+            self.connectedWorkers[worker].connect("tcp://127.0.0.1:"+str(workerPortLocation[worker]))
+
+
         
 
-    
-    
-    
         
+
+    #Generic Methods
+    def sendCommand(self, command):
+        dicCommand = {'command':command}
+        for worker in self.connectedWorkers:
+            self.connectedWorkers[worker].send_pyobj(dicCommand)
+
+
+    def test(self):
+        self.sendCommand("test")
+        time.sleep(0.1)
+        self.sendCommand("shut_down")
+        time.sleep(0.1)
 
 
 if __name__ == "__main__":
     eng = Engine2("settings.conf")
+    eng.test()
 
 
 
